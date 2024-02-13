ce02b2957c9a93670460039dee528ac4a96643ac
Yitong Wu <wuyt26@outlook.com>
2024-02-12 20:29:48 -0800

    demo 8-10

---
+++ playground/src/components/Demos/Demo10.jsx
@@ -0,0 +1,122 @@
+import React, { useEffect, useRef } from 'react';
+import * as THREE from 'three';
+
+function Demo10() {
+  const mountRef = useRef(null);
+
+  useEffect(() => {
+    const currMountRef = mountRef.current;
+
+    const vshader = /*glsl*/ `
+      varying vec3 vPosition;
+
+      void main() {	
+        vPosition = position;
+        gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+      }
+      `;
+
+    const fshader = /*glsl*/ `
+      #define PI2 6.28318530718
+
+      uniform vec2 u_resolution;
+      uniform float u_time;
+      uniform vec3 u_color;
+      
+      varying vec3 vPosition;
+      
+      float rect(vec2 pt, vec2 anchor, vec2 size, vec2 center){
+        vec2 halfsize = size * 0.5;
+        vec2 p = pt - center;
+        float horz = step(-halfsize.x - anchor.x, p.x) - step(halfsize.x - anchor.x, p.x);
+        float vert = step(-halfsize.y - anchor.y, p.y) - step(halfsize.y - anchor.y, p.y);
+        return horz * vert;
+      }
+
+      mat2 getRotationMatrix(float theta){
+        float s = sin(theta);
+        float c = cos(theta);
+        return mat2(c, -s, s, c);
+      }
+
+      mat2 getScaleMatrix(float scale){
+        return mat2(scale, 0, 0, scale);
+      }
+      
+      void main (void) {
+        vec2 adjustedCoords = vPosition.xy * u_resolution / min(u_resolution.x, u_resolution.y);
+        vec2 center = vec2(0.1, 0.3);
+        mat2 matr = getRotationMatrix(u_time);
+        mat2 mats = getScaleMatrix((sin(u_time)+1.0)/3.0 + 0.5);
+        vec2 pt = adjustedCoords - center;
+        pt = mats * matr * pt;
+        pt += center;
+        vec3 color = u_color * rect(pt, vec2(0.0), vec2(0.3), center);
+        gl_FragColor = vec4(color, 1.0); 
+      }
+    `;
+
+    const scene = new THREE.Scene();
+    const camera = new THREE.OrthographicCamera(-1, 1, 1, -1, 0.1, 10);
+
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    mountRef.current.appendChild(renderer.domElement);
+
+    const clock = new THREE.Clock();
+
+    const geometry = new THREE.PlaneGeometry(2, 2);
+
+    const uniforms = {
+      u_color: { value: new THREE.Color(0xffff00) },
+      u_time: { value: 0.0 },
+      u_resolution: { value: { x: 0, y: 0 } }
+    };
+
+    const material = new THREE.ShaderMaterial({
+      uniforms: uniforms,
+      vertexShader: vshader,
+      fragmentShader: fshader
+    });
+
+    const plane = new THREE.Mesh(geometry, material);
+    scene.add(plane);
+
+    camera.position.z = 1;
+
+    function animate() {
+      requestAnimationFrame(animate);
+      uniforms.u_time.value += clock.getDelta();
+      renderer.render(scene, camera);
+    }
+    animate();
+
+    let resizeObserver;
+    if (ResizeObserver) {
+      resizeObserver = new ResizeObserver((entries) => {
+        for (let entry of entries) {
+          const { width, height } = entry.contentRect;
+          camera.aspect = width / height;
+          camera.updateProjectionMatrix();
+          renderer.setSize(width, height);
+          uniforms.u_resolution.value.x = width;
+          uniforms.u_resolution.value.y = height;
+        }
+      });
+      resizeObserver.observe(mountRef.current);
+    }
+
+    return () => {
+      if (resizeObserver && currMountRef) {
+        resizeObserver.unobserve(currMountRef);
+      }
+      if (currMountRef) {
+        currMountRef.removeChild(renderer.domElement);
+      }
+    };
+  }, []);
+
+  return <div ref={mountRef} style={{ width: '100%', height: '100%' }}></div>;
+}
+
+export default Demo10;
--- playground/src/store/dirs.js
+++ playground/src/store/dirs.js
@@ -6,6 +6,8 @@ import Demo5 from '../components/Demos/Demo5';
 import Demo6 from '../components/Demos/Demo6';
 import Demo7 from '../components/Demos/Demo7';
 import Demo8 from '../components/Demos/Demo8';
+import Demo9 from '../components/Demos/Demo9';
+import Demo10 from '../components/Demos/Demo10';
 
 const dirs = {
   'First Shader': Demo1,
@@ -15,7 +17,9 @@ const dirs = {
   'Using Clamp 2': Demo5,
   'Using Step': Demo6,
   'Using Smoothstep': Demo7,
-  'Moving Square': Demo8
+  'Moving Square': Demo8,
+  'Rotating Square': Demo9,
+  'Scaling Square': Demo10
 };
 
 export default dirs;
